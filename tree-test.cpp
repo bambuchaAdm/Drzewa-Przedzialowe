c4e45563d08938afd2aa1823df7c81a812cc24d3
Łukasz Dubiel <bambucha14@gmail.com>
2012-05-28 03:00:04 +0200

    Dodanie nagłówka i plików z testami

---
+++ tree-test.cpp
@@ -0,0 +1,8 @@
+/**
+ *
+ * Plik zawiera testy implementacji drzewa przedziałowego.
+ * Jako jeden z najniższych kawałków kodu byłoby miło gdyby zawsze poprawnie działało.
+ * 
+ */
+
+#include <gtest/gtest.h>
---
+++ tree.hpp
@@ -0,0 +1,4 @@
+#ifndef __INTERVAL_TREE_HPP__
+#define __INTERVAL_TREE_HPP__
+
+#endif /* __INTERVAL_TREE_HPP__ */
